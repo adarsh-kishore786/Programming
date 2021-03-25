d92c08d9277336655990a0dad74dbe3bf3a35e63
ashy-boy <adarshkishore.201it103@nitk.edu.in>
2021-03-25 10:23:40 +0530

    Learning on 25/03/2021

---
+++ C/file_char_data.c
@@ -0,0 +1,70 @@
+/*
+	file_char_data.c
+
+	Reads a file and prints number of characters,
+	tabs, space, newline, alphabets, digits, special
+	characters.
+*/
+
+#include <stdio.h>
+#include <ctype.h>
+
+typedef struct file_data
+{
+	int num_char;
+	int num_newline;
+	int num_tab;
+	int num_space;
+	int num_alpha;
+	int num_digit;
+	int num_special;
+} file_data;
+
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		printf("No/invalid filename given. Try again.\n");
+		return 1;
+	}
+
+	FILE *fp = fopen(argv[1], "r");
+
+	if (!fp)
+	{
+		printf("No such file exists. Try again.\n");
+		return 2;
+	}
+
+	char ch;
+	file_data fd = {0}; // 0 initialization to everything
+
+	while ((ch = fgetc(fp)) != EOF)
+	{
+		printf("%c", ch);
+		fd.num_char++;
+
+		if (ch == '\n')
+			fd.num_newline++;
+		else if (ch == '\t')
+			fd.num_tab++;
+		else if (ch == ' ')
+			fd.num_space++;
+		else if (isalpha(ch))
+			fd.num_alpha++;
+		else if (isdigit(ch))
+			fd.num_digit++;
+		else
+			fd.num_special++;
+	}
+
+	printf("\n\n");
+	printf("Number of characters: %d\n", fd.num_char);
+	printf("Number of newlines  : %d\n", fd.num_newline);
+	printf("Number of tabs      : %d\n", fd.num_tab);
+	printf("Number of alphabets : %d\n", fd.num_alpha);
+	printf("Number of digits    : %d\n", fd.num_digit);
+	printf("Number of special ch: %d\n", fd.num_special);
+
+	return 0;
+}
--- C/file_read.c
+++ C/file_read.c
@@ -1,26 +1,33 @@
-/* file_read.c
+/*
+	file_read.c
 
-   This program creates a file
-   IO app.
-
-   Author: Adarsh
+	Displays contents of a file
 */
 
 #include <stdio.h>
 
-int main()
+int main(int args, char *argv[])
 {
-	FILE *fp;
-	char ch;
-	fp = fopen("file_read.txt", "r");
-
-	while (1)
+	if (args != 2)
 	{
-		ch = fgetc(fp);
-		if (ch == EOF)
-			break;
-		printf("%c", ch);
+		printf("Invalid or no filename given. Try again.\n");
+		return 1;
 	}
+
+	FILE *fp;
+	char ch = ' ';
+	fp = fopen(argv[1], "r");
+
+	if (!fp)
+	{
+		printf("No such file exists. Try again.\n");
+		return 2;
+	}
+
+	while ((ch = fgetc(fp)) != EOF)
+		printf("%c", ch);
+
 	printf("\n");
 	fclose(fp);
+	return 0;
 }
