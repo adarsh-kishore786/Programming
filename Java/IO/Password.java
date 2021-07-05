dfbbd0c9f40cd21758061e5198478b3e5636ea85
Adarsh Kishore <adarshkishore201it103@nitk.edu.in>
2021-07-05 19:42:08 +0530

    Some Changes

--- Java/IO/Password.java
+++ Java/IO/Password.java
@@ -70,7 +70,6 @@ public class Password
                         sc.close();
                         return true;
                     }
-                    break;
                 }
             }
         }
