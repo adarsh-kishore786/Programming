8885098a648d48b00a5113350aedbdcad9756289
adarsh.kishore786@gmail.com <adarsh.kishore786@gmail.com>
2021-09-02 17:25:30 +0530

    Char Module

--- Haskell/char_module.hs
+++ Haskell/char_module.hs
@@ -21,3 +21,13 @@ encode shift = map (chr . (+shift) . ord)
 
 decode :: Int -> String -> String
 decode shift = encode (negate shift)
+
+shift_char :: Int -> Char -> Char
+shift_char shift char
+    | (isUpper char) || (isLower char) =
+        let base = if isUpper char then 65 else 97
+        in chr $ ((ord char - base + shift) `mod` 26) + base
+    | otherwise = char
+
+caesar_shift :: Int -> String -> String
+caesar_shift shift str = map (shift_char shift) str
