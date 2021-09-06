dd028b85f93158553c53b37cc5be60b161631e93
adarsh.kishore786@gmail.com <adarsh.kishore786@gmail.com>
2021-09-06 18:36:43 +0530

    Map module in Haskell

--- Haskell/map_module.hs
+++ Haskell/map_module.hs
@@ -1,4 +1,5 @@
 import qualified Data.Map as Map
+import Data.Char
 
 phoneBook =
     Map.fromList
@@ -29,4 +30,43 @@ findKey key (ft:rest)
 size' = Map.size phoneBook
 
 -- singleton takes a single (k, v) and creates a singleton map
-map' = Map.insert 6 7 $ Map.singleton 3 9 
+map' = Map.insert 6 7 $ Map.singleton 3 9
+
+-- Map.lookup works like the normal lookup of lists, returns Something or
+-- Nothing
+
+-- Map.map and Map.filter work like their list equivalents.
+map'' = Map.map (/100) $ Map.fromList [(1, 10), (2, 20), (3, 50)]
+map_ = Map.filter isUpper $ Map.fromList [(1, 'a'), (2, 'B'), (3, '*'),
+            (4, 'G')]
+
+-- toList is just inverse of fromList
+list' = Map.toList $ Map.fromList [(1, 'a'), (2, 'b'), (3, 'c')]
+
+-- Map.keys and Map.elems return the keys and values respectively of a
+-- map
+-- Map.keys m = map fst . Map.toList m
+-- Map.elems m = map snd . Map.toList m
+
+-- fromListWith reserves the duplicates
+phoneBook' =
+    [("betty","555-2938")
+    ,("betty","342-2492")
+    ,("bonnie","452-2928")
+    ,("patsy","493-2928")
+    ,("patsy","943-2929")
+    ,("patsy","827-9162")
+    ,("lucille","205-2928")
+    ,("wendy","939-8282")
+    ,("penny","853-2492")
+    ,("penny","555-2111")
+    ]
+
+phoneBookToMap :: (Ord k) => [(k, String)] -> Map.Map k String
+phoneBookToMap xs = Map.fromListWith (\num1 num2 -> num1 ++ ", " ++ num2) xs
+-- phoneBookToMap :: (Ord k) => [(k, a)] -> Map.Map k [a]
+-- phoneBookToMap xs = Map.fromListWith (++) $ map (\(k, v) -> (k, [v])) xs
+
+-- insertWith : insert :: fromListWith : fromList. It inserts a key and if
+-- that key exists already, it provides a function to accomodate the new value
+map_' = Map.insertWith (+) 3 100 $ Map.fromList [(3, 4), (5, 103), (6, 339)]
