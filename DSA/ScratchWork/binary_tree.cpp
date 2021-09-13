6165910b929d431b7dfe4c7b803288c86a04f3ec
adarsh.kishore786@gmail.com <adarsh.kishore786@gmail.com>
2021-09-13 16:20:09 +0530

    Binary Tree: Priniting and Searching

--- DSA/ScratchWork/binary_tree.cpp
+++ DSA/ScratchWork/binary_tree.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <algorithm>
 #include <exception>
+#include <string>
 
 using dType = int;
 
@@ -12,20 +13,26 @@ struct Node
     Node<T>* leftNode;
 };
 
-template <typename T>
-int getHeight(Node<T>* root)
-{
-    if (!root)
-        return 0;
-
-    return 1 + std::max(getHeight(root->leftNode), getHeight(root->rightNode));
-}
-
 template <class T>
 class BinaryTree
 {
 private:
     Node<T>* root = nullptr;
+
+    static int getHeight(Node<T>* root)
+    {
+        if (!root)
+            return 0;
+
+        return 1 + std::max(getHeight(root->leftNode), getHeight(root->rightNode));
+    }
+
+    static const std::string print(Node<T>* node)
+    {
+        if (!node)
+            return "Empty";
+        return "" + std::to_string(node->val) + " (" + print(node->leftNode) + ") (" + print(node->rightNode) + ")";
+    }
 
 public:
     BinaryTree() {}
@@ -79,7 +86,23 @@ public:
 
     friend std::ostream& operator<<(std::ostream& os, const BinaryTree<T>& bt)
     {
+        os << print(bt.root) << "\n";
         return os;
+    }
+
+    bool search(T val)
+    {
+        Node<T>* currNode = root;
+        while (currNode)
+        {
+            if (currNode->val == val)
+                return true;
+            else if (currNode->val < val)
+                currNode = currNode->rightNode;
+            else
+                currNode = currNode->leftNode;
+        }
+        return false;
     }
 };
 
@@ -94,6 +117,19 @@ void insert_element(BinaryTree<T>& root)
     std::cout << "Successfully inserted " << value << "!\n\n";
 }
 
+template <typename T>
+void search_element(BinaryTree<T>& root)
+{
+    std::cout << "Enter value to insert: ";
+    T value {};
+    std::cin >> value;
+
+    if (root.search(value))
+        std::cout << "Value exists in tree.\n\n";
+    else
+        std::cout <<  "Value does not exist in tree.\n\n";
+}
+
 int main()
 {
     BinaryTree<dType> root{};
@@ -105,7 +141,9 @@ int main()
             std::cout << "What would you like to do?\n";
             std::cout << "1. Insert an element\n";
             std::cout << "2. Get height of tree\n";
-            std::cout << "3. Quit\n";
+            std::cout << "3. Print the tree\n";
+            std::cout << "4. Search for an element in the tree\n";
+            std::cout << "5. Quit\n";
             std::cout << "Enter your choice.\n";
 
             int ch {};
@@ -117,7 +155,11 @@ int main()
                         break;
                 case 2: std::cout << "Height is " << root.height() << ".\n\n";
                         break;
-                case 3: return 0;
+                case 3: std::cout << "Tree is:\n" << root << "\n";
+                        break;
+                case 4: search_element(root);
+                        break;
+                case 5: return 0;
                 default: std::cout << "Invalid option. Try again.\n\n";
             }
         }
