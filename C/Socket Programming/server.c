b878cd633d1f682f05a65d7cc49f719ec85ac34c
adarsh.kishore786@gmail.com <adarsh.kishore786@gmail.com>
2021-10-07 17:50:17 +0530

    Server-Client Architecture

--- C/Socket Programming/server.c
+++ C/Socket Programming/server.c
@@ -53,7 +53,7 @@ int main(int argc, char *argv[]) {
 
         if (n < 0) error("Error on reading");
 
-        printf("Client: %s\n", buffer);
+        printf("Client: %s", buffer);
         bzero(buffer, 255);
         fgets(buffer, 255, stdin);
 
