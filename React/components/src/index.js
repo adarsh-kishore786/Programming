791b8d704bf6e04235873e402dc65a7d4d419b2a
adarsh.kishore786@gmail.com <adarsh.kishore786@gmail.com>
2021-11-04 15:18:46 +0530

    React: Reusing Components

--- React/components/src/index.js
+++ React/components/src/index.js
@@ -1,29 +1,45 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 import faker from "faker";
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail
-                author="Sam"
-                timeAgo="Today at 4:45PM"
-                comText="Nice blog!"
-                profileImg={faker.image.avatar()}
-            />
-            <CommentDetail
-                author="Alex"
-                timeAgo="Today at 2:00AM"
-                comText="Motivating!"
-                profileImg={faker.image.avatar()}
-            />
-            <CommentDetail
-                author="Jane"
-                timeAgo="Yesterday at 6:00PM"
-                comText="We need more people like you"
-                profileImg={faker.image.avatar()}
-            />
+            <ApprovalCard>
+                <div>
+                    <h4>Warning</h4>
+                    Are you sure you want to do this?
+                </div>
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail
+                    author="Sam"
+                    timeAgo="Today at 4:45PM"
+                    comText="Nice blog!"
+                    profileImg={faker.image.avatar()}
+                />
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail
+                    author="Alex"
+                    timeAgo="Today at 2:00AM"
+                    comText="Motivating!"
+                    profileImg={faker.image.avatar()}
+                />
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail
+                    author="Jane"
+                    timeAgo="Yesterday at 6:00PM"
+                    comText="We need more people like you"
+                    profileImg={faker.image.avatar()}
+                />
+            </ApprovalCard>
         </div>
     );
 };
