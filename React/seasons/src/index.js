a9eb5e9867eae2f3ed1fb2446b78c1d8f76a7ab4
adarsh.kishore786@gmail.com <adarsh.kishore786@gmail.com>
2021-11-13 08:55:00 +0530

    React Tutorial 13/11

--- React/seasons/src/index.js
+++ React/seasons/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
 class App extends React.Component {
     state = { lat: null, errorMessage: "" };
@@ -12,12 +13,20 @@ class App extends React.Component {
         );
     }
 
-    render() {
+    renderContent() {
         if (this.state.error && !this.state.lat)
-            return <div>Error: {this.state.errorMessage}</div>;
+        return <div>Error: {this.state.errorMessage}</div>;
         if (!this.state.error && this.state.lat)
-            return <SeasonDisplay lat={this.state.lat} />;
-        return <div>Loading!</div>
+        return <SeasonDisplay lat={this.state.lat} />;
+        return <Spinner message="Please accept location request" />;
+    }
+
+    render() {
+        return(
+            <div className="border red">
+                {this.renderContent()}
+            </div>
+        );
     }
 }
 
