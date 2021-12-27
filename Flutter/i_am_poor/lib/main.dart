032c1fd6198449f0530835782505699e2f972036
adarsh.kishore786@gmail.com <adarsh.kishore786@gmail.com>
2021-12-27 21:07:17 +0530

    Flutter Dev: I am poor app

---
+++ Flutter/i_am_poor/lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('I am Poor'),
+        backgroundColor: Color.fromRGBO(48, 10, 36, 1),
+        centerTitle: true,
+        elevation: 10,
+        foregroundColor: Colors.blueAccent,
+      ),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Image(
+                image: AssetImage("images/top-left.png"),
+              ),
+            ],
+          ),
+          Center(
+            child: Image(
+              image: AssetImage("images/bottom.png"),
+            ),
+          )
+        ]
+      ),
+      backgroundColor: Colors.green[600]
+    ),
+  ));
+}
