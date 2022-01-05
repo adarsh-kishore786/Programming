b5a5b9b7ea204f9bb6d96a101fb35836b1a317df
ashy-boy786 <adarsh.kishore786@gmail.com>
2022-01-05 22:32:48 +0530

    Flutter Dev: 05/01

--- Flutter/mi_card_flutter/lib/main.dart
+++ Flutter/mi_card_flutter/lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               CircleAvatar(
                 radius: 50.0,
@@ -34,6 +35,48 @@ class MyApp extends StatelessWidget {
                   fontSize: 20.0,
                   letterSpacing: 2.5,
                   fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.teal[100],
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal[900],
+                  ),
+                  title: Text(
+                    "+91 9523321247",
+                    style: TextStyle(
+                        color: Colors.teal[900],
+                        fontSize: 20.0,
+                        fontFamily: "SourceSansPro"
+                    ),
+                  ),
+                ),
+                ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal[900],
+                  ),
+                  title: Text(
+                    "adarsh.kishore786@gmail.com",
+                    style: TextStyle(
+                      fontSize: 17.0,
+                      fontFamily: "SourceSansPro",
+                      color: Colors.teal[900],
+                    ),
+                  ),
                 ),
               ),
             ],
